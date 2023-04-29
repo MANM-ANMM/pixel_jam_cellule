570dfb1218eae8f8fcafc4ba591f2e5db96d25bb
Martin Rouault <mrouault@hotmail.fr>
2023-04-29 19:58:58 +0200

    Ca marche

--- script/puzzle.gd
+++ script/puzzle.gd
@@ -5,22 +5,18 @@ extends Node2D
 @onready var tile_map :TileMapPuzzle= $TileMap
 
 func test_chemin(depart:Vector2i,arriver:Vector2i)->bool:
-	var deplacement_prec := Enums.Direction.Droite 
-	var case_actuelle := depart
+	var deplacement_prec := Enums.Direction.Droite
+	var case_actuelle := depart + Enums.direction_to_vec(Enums.Direction.Droite)
 	
 	while true:
-		print(case_actuelle)
 		if case_actuelle == arriver:
 			break
 		
 		var dirs := tile_map.get_directions_tube(case_actuelle)
-		print(dirs)
-		print(Enums.direction_oposee(deplacement_prec))
 		if not dirs.has(Enums.direction_oposee(deplacement_prec)):
 			return false
 		
 		dirs = dirs.filter(func(d): return d != Enums.direction_oposee(deplacement_prec))
-		print(dirs)
 		if dirs.size() == 0:
 			return false
 		elif dirs.size() == 1:
@@ -36,7 +32,7 @@ func _on_tile_map_cell_clicked(pos:Vector2i):
 	if not((pos.x>-1 and pos.x<taille.x) and (pos.y>-1 and pos.y <taille.y)):
 		coord1 = Vector2i(-1,-1)
 		return
-	print(pos)
+	
 	if coord1 != Vector2i(-1,-1):
 		if tile_map.adjacence(pos,coord1):
 			tile_map.deplacer_cellule(coord1,pos)
@@ -56,10 +52,8 @@ func chercher_arriver(couleur:int,tableau:Array)->int:
 func tuyau_relier()->bool:
 	var tabdepart := tile_map.get_entrees_tubes()
 	var tabarriver := tile_map.get_sorties_tubes()
-	var idarr := -1
-	for dep in tabdepart :
-		idarr = chercher_arriver(dep[1],tabarriver)
-		if not(test_chemin(dep[0],tabarriver[idarr][0])):
+	for dep in tabdepart:
+		if not test_chemin(tabdepart[dep], tabarriver[dep]):
 			return false
 	return true
 	
--- script/tilemap_puzzle.gd
+++ script/tilemap_puzzle.gd
@@ -5,30 +5,32 @@ signal cell_clicked(Vector2i)
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
-		print(get_entrees_tubes())
 		if event.pressed:
 			var cliked_cell = local_to_map(to_local(event.global_position))
 			emit_signal("cell_clicked", cliked_cell)
 
 func get_directions_tube(coord:Vector2i)->Array:
 	var atlas_coords := get_cell_atlas_coords(0, coord)
-	print(atlas_coords, coord)
 	if Constantes.entrees_id.has(atlas_coords):
 		return Constantes.entrees_id[atlas_coords]
 	else:
 		return []
 
 
-func get_entrees_tubes()->Array:
+func get_entrees_tubes()->Dictionary:
 	return get_entrees_sorties_helper(Constantes.couleur_tube_entree_id)
 
-func get_sorties_tubes()->Array:
+func get_sorties_tubes()->Dictionary:
 	return get_entrees_sorties_helper(Constantes.couleur_tube_sortie_id)
 
-func get_entrees_sorties_helper(tab_id_couleur)->Array:
-	return get_used_cells(0) \
-		.filter(func(c): return tab_id_couleur.has(get_cell_atlas_coords(0, c))) \
-		.map(func(c): return {c : tab_id_couleur[get_cell_atlas_coords(0, c)]})
+func get_entrees_sorties_helper(tab_id_couleur)->Dictionary:
+	var dict:Dictionary
+	var cells = get_used_cells(0) \
+		.filter(func(c): return tab_id_couleur.has(get_cell_atlas_coords(0, c)))
+	
+	for c in cells:
+		dict[tab_id_couleur[get_cell_atlas_coords(0, c)]] = c
+	return dict
 
 func is_there_cell(coord:Vector2i)->bool:
 	var pre := get_cell_source_id(0, coord)
