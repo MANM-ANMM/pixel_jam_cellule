096c25dc793f8a3750df720066314fe0e060d724
Martin Rouault <mrouault@hotmail.fr>
2023-04-30 11:25:13 +0200

    Routine garde

--- script/gardien.gd
+++ script/gardien.gd
@@ -1,25 +1,38 @@
 extends Node2D
 
-@export var points_arrets:Array[Marker2D]
-@export var points_extremes:Array[Marker2D]
+@export var points_arrets:Array[NodePath]
+@export var points_extremes:Array[NodePath]
 
 @onready var timer := $Timer
+@onready var sprite := $Icon
 
 func _ready():
-	timer.start(randf_range(10, 20))
+	timer.start(randf_range(5, 15))
 
 func avance_vers(depart:float, arrivee:float, temps:float):
 	position.x = depart
-	$Icon.flip_h = depart > arrivee
-	$Icon.play("default")
+	sprite.flip_h = depart > arrivee
+	sprite.play("default")
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "position", Vector2(arrivee, position.y), temps)
-	tween.tween_callback($Icon.stop)
+	tween.tween_callback(sprite.stop)
 
 
 func _on_timer_timeout():
 	var tween = get_tree().create_tween()
-	var deb : Vector2 = points_extremes.pick_random().position
-	var arrivee : Vector2 = points_extremes.pick_random().position
-	var arret : Vector2 = points_arrets.pick_random().position
+	var deb : Vector2 = get_node(points_extremes.pick_random()).position
+	var arrivee : Vector2 = get_node(points_extremes.pick_random()).position
+	var arret : Vector2 = get_node(points_arrets.pick_random()).position
 	
+	position = deb
+	sprite.flip_h = deb.x > arret.x
+	sprite.play("default")
+	tween.tween_property(self, "position", arret, randf_range(2.0, 10.0))
+	tween.tween_callback(sprite.stop)
+	tween.tween_interval(randf_range(1.0, 3.0))
+	tween.tween_callback(func():sprite.play("default"))
+	tween.tween_callback(func():sprite.flip_h = arret.x > arrivee.x)
+	tween.tween_property(self, "position", arrivee, 5.0)
+	tween.tween_callback(sprite.stop)
+
+	tween.tween_callback(func():timer.start(randf_range(5, 15.0)))
